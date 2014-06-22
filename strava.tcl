a061c3a3f8181e8c957c0c09cb69ec028667e595
Will Storey <will@summercat.com>
2014-06-22 13:23:13 -0700

    add leaderboard trigger (elevation only currently)

--- strava.tcl
+++ strava.tcl
@@ -45,13 +45,22 @@ namespace eval ::strava {
 	# http timeout. in seconds.
 	variable http_timeout 5
 
+	# number of pages of activities to request at once.
+	variable per_page 30
+
 	# this is an internal counter to track the highest activity seen.
 	variable club_activity_id 0
 
 	# cache leaderboard for 1 hour.
-	# TODO: this feature is incomplete.
+	# TODO: caching feature is incomplete.
 	variable leaderboard_cache_length 3600
 	variable leaderboard_cache_time 0
+
+	# how many days back to generate a leaderboard for.
+	variable leaderboard_days 14
+
+	# how many requests maximum to make when building a single leaderboard.
+	variable leaderboard_max_requests 2
 
 	# add a configuration option to set what channels are active for
 	# channel triggers.
@@ -59,6 +68,9 @@ namespace eval ::strava {
 
 	# add a channel trigger. TODO: this feature is incomplete.
 	signal_add msg_pub !clubs ::strava::clubs
+
+	# add a channel for retrieving leader board for the club.
+	signal_add msg_pub .leaderboard ::strava::leaderboard
 }
 
 # load a config from disk if one is present.
@@ -166,6 +178,8 @@ proc ::strava::show {club_activities} {
 			continue
 		}
 
+		# TODO: check each key we want in the dict is there - it's an error to get
+		#   one and it not be present.
 		set id [dict get $activity "id"]
 		if {$::strava::club_activity_id >= $id} {
 			continue
@@ -239,6 +253,253 @@ proc ::strava::clubs {server nick uhost chan argv} {
 	}
 }
 
+# determine if an activity falls in our leaderboard time range.
+#
+# if an activity is more recent than leaderboard_days ago, we return 1.
+# otherwise we return 0.
+#
+# parameters:
+# activity - an activity dict.
+#
+# returns integer: 1 if activity should be included, 0 if not.
+#
+# SIDE EFFECT: throws an error if there is an error!
+proc ::strava::activity_is_in_leaderboard {activity} {
+		# check dict is valid.
+		if {[catch {dict size $activity}]} {
+			irssi_print "activity_is_in_leaderboard: invalid dict found"
+			error "invalid activity dict found"
+		}
+		# get its date and find out how many days ago it was.
+		if {![dict exists $activity start_date]} {
+			irssi_print "activity_is_in_leaderboard: activity dict is missing start_date!"
+			error "activity dict does not have start_date"
+		}
+		set start_date [dict get $activity start_date]
+		# TODO: is this actually GMT? docs seem ambiguous.
+		set start_date_unixtime [clock scan $start_date \
+			-format "%Y-%m-%dT%H:%M:%SZ" -timezone :GMT]
+		set diff [expr [clock seconds] - $start_date_unixtime]
+		if {[expr $diff >= [expr $::strava::leaderboard_days * 24 * 60 * 60]]} {
+			return 0
+		}
+		return 1
+}
+
+# generate and output the leaderboard to the IRC channel.
+#
+# parameters:
+# server - the server to send leaderboard to
+# chan - the channel to send leaderboard to
+# activities - list of activities retrieved to build the leaderboard.
+#
+# returns nothing.
+proc ::strava::leaderboard_output {server chan activities} {
+	# pull out the totals for each athlete.
+	set elevations [dict create]
+	foreach activity $activities {
+		# NOTE: we assume the dict is valid at this point!
+		if {![::strava::activity_is_in_leaderboard $activity]} {
+			continue
+		}
+		# get the athlete name as a unique identifier for them.
+		# TODO: first name is not a very unique key if we have a big club!
+		if {![dict exists $activity athlete]} {
+			irssi_print "leaderboard_output: activity is missing athlete!"
+			return
+		}
+		set athlete [dict get $activity athlete]
+		if {![dict exists $athlete firstname]} {
+			irssi_print "leaderboard_output: athlete is missing firstname!"
+			return
+		}
+		set name [dict get $athlete firstname]
+		if {[expr [string length $name] == 0]} {
+			irssi_print "leaderboard_output: athlete name is blank!"
+			return
+		}
+		# elevation.
+		if {![dict exists $elevations $name]} {
+			dict set elevations $name 0
+		}
+		if {![dict exists $activity total_elevation_gain]} {
+			irssi_print "leaderboard_output: total_elevation_gain is missing!"
+			return
+		}
+		set climb [::tcl::mathfunc::int [dict get $activity total_elevation_gain]]
+		dict incr elevations $name $climb
+	}
+	# sort them. I convert to a list so we can use lsort.
+	set elevations_list [list]
+	foreach athlete [dict keys $elevations] {
+		lappend elevations_list [list $athlete [dict get $elevations $athlete]]
+	}
+	set elevs_sorted [lsort -integer -decreasing -index 1 $elevations_list]
+	putchan $server $chan "Leaderboard (elevation) for the past $::strava::leaderboard_days days:"
+	set i 0
+	foreach elev $elevs_sorted {
+		lassign $elev athlete elevation
+		incr i
+		if {[expr $i > 10]} {
+			break
+		}
+		set output "$i. $athlete @ ${elevation}m"
+		putchan $server $chan $output
+	}
+	if {[expr [llength $elevs_sorted] == 0]} {
+		putchan $server $chan "No athletes!"
+	}
+}
+
+# handle a callback from an API request to build a leaderboard.
+#
+# see leaderboard_cb.
+proc ::strava::_leaderboard_cb {server chan activities page request_count token} \
+{
+	# check the response
+	set status [::http::status $token]
+	if {![string match $status ok]} {
+		irssi_print "_leaderboard_cb: HTTP request problem"
+		::http::cleanup $token
+		return
+	}
+	# parse out the activities.
+	set data [::http::data $token]
+	::http::cleanup $token
+	set new_activities [::json::json2dict $data]
+	# we have a list of activities now... not a dict yet, I think... so check the
+	# dict as we iterate rather than checking $new_activities itself.
+	foreach activity $new_activities {
+		# check we have a valid dict.
+		if {[catch {dict size $activity}]} {
+			irssi_print "_leaderboard_cb: HTTP response is not valid json"
+			return
+		}
+		# TODO: should we check the activity isn't already in our list somehow?
+		#   possible race condition here... for example if an activity shows up on
+		#   two pages we requested.
+		lappend activities $activity
+	}
+
+	# now determine if we have enough activities to stop and generate the output
+	# or if we have to start a new request to get more activities
+	# we can stop if we received less than per_page activities (as that means we
+	# hit the end of activities), or if we have an activity that's more than
+	# leaderboard_days days back (because we only need activities up to that
+	# point).
+	if {[expr [llength $new_activities] < $::strava::per_page]} {
+		irssi_print "_leaderboard_cb: hit end of activities"
+		::strava::leaderboard_output $server $chan $activities
+		return
+	}
+	# check if there's an activity that is outside of our leaderboard range. if
+	# so, we can stop.
+	foreach activity $activities {
+		if {![::strava::activity_is_in_leaderboard $activity]} {
+			irssi_print "_leaderboard_cb: found activity that is old enough, done!"
+			::strava::leaderboard_output $server $chan $activities
+			return
+		}
+	}
+	# we also do not make any more API requests if we're at our maximum number of
+	# requests already.
+	if {[expr $request_count >= $::strava::leaderboard_max_requests]} {
+		irssi_print "_leaderboard_cb: max API request count hit"
+		::strava::leaderboard_output $server $chan $activities
+		return
+	}
+	# we need to start a new request for more activities it seems.
+	incr page
+	::strava::leaderboard_api_request $server $chan $activities $page \
+		$request_count
+}
+
+# callback from an http request to build a leaderboard.
+#
+# if we have enough activities for this leaderboard then we'll send the output.
+# otherwise we'll initiate another API request.
+#
+# params:
+# in the params list:
+#   server - the server to send leaderboard to
+#   chan - the channel to send leaderboard to
+#   activities - list of activities retrieved so far. may be empty.
+#     this is necessary because we may need to retrieve multiple pages of
+#     activities and so this will hold those we've received so far.
+#   page - the page we're on of activities
+#   request_count - how many api requests we've made so far.
+# token - ::http token
+#
+# returns nothing
+proc ::strava::leaderboard_cb {params token} {
+	lassign $params server chan activities page request_count
+	# why is this structured this way? because due to us using async requests
+	# we can 'lose' errors and make for difficult debugging if we don't
+	# catch errors somewhere. it's a limitation with ::http::geturl that I haven't
+	# figured a better solution for than this.
+	if {[catch {::strava::_leaderboard_cb $server $chan $activities $page \
+		$request_count $token} err]} \
+	{
+		irssi_print "leaderboard_cb: encountered error: $err"
+	}
+}
+
+# initiate a new leaderboard API request.
+#
+# we expect we really do want to make an api request (though we'll check that
+# request_count is within range just in case). otherwise we don't double check
+# whether there are enough activities to output already.
+#
+# parameters:
+# server - the server to send leaderboard to
+# chan - the channel to send leaderboard to
+# activities - list of activities retrieved so far. may be empty.
+#   this is necessary because we may need to retrieve multiple pages of
+#   activities and so this will hold those we've received so far.
+# page - the page we're on of activities
+# request_count - how many api requests we've made so far.
+#
+# returns void.
+proc ::strava::leaderboard_api_request {server chan activities page request_count} {
+	if {[expr $request_count >= $::strava::leaderboard_max_requests]} {
+		irssi_print "leaderboard_api_request: already at maximum number of requests!"
+		return
+	}
+	irssi_print "leaderboard_api_request: requesting page $page"
+	set url [join [list $::strava::base_url "clubs" $::strava::club_id "activities"] "/"]
+	set params [::http::formatQuery page $page per_page $::strava::per_page]
+	append url "?$params"
+	incr request_count
+
+	# TODO: are there escaping issues here with this callback?
+	set params [list [list $server $chan $activities $page $request_count]]
+	set cb "::strava::leaderboard_cb $params"
+	::strava::api_request $url $cb
+}
+
+# channel trigger to retrieve the leaderboard for the club.
+#
+# we will start an API request to determine this.
+#
+# parameters:
+# server - server where trigger initiated
+# nick - who initiated the trigger
+# uhost - userhost of who initiated the trigger
+# chan - channel where trigger initiated
+# argv - arguments to the trigger
+#
+# returns nothing.
+proc ::strava::leaderboard {server nick uhost chan argv} {
+	if {![str_in_settings_str "strava_enabled_channels" $chan]} {
+		return
+	}
+	set activities [list]
+	set page 1
+	set request_count 0
+	::strava::leaderboard_api_request $server $chan $activities $page \
+		$request_count
+}
+
 # this proc is a callback from ::http::geturl called asynchronously
 # in ::strava::club_activities.
 #
@@ -259,8 +520,9 @@ proc ::strava::club_activities_cb {token} {
 
 	set activities [::json::json2dict $data]
 	# check that we have a valid dict.
+	# TODO: we don't have a dict here, do we? we should have a list.
 	if {[catch {dict size $activities}]} {
-		irssi_print "club_activities_cb: HTTP request is not valid json"
+		irssi_print "club_activities_cb: HTTP response is not valid json"
 		return
 	}
 
@@ -274,30 +536,43 @@ proc ::strava::club_activities_cb {token} {
 	set ::strava::club_activity_id [::strava::get_highest_id $activities]
 }
 
-# start an http request to retrieve and output club activities.
+# start an http request to the strava API.
+#
 # this is asynchronous so we don't block.
 #
+# parameters:
+# url - url to request to
+# cb - callback function to run on http request completion
+#
 # returns nothing.
-proc ::strava::club_activities {} {
+proc ::strava::api_request {url cb} {
 	if {[expr [string length $::strava::oauth_token] == 0]} {
-		irssi_print "strava: club_activities: cannot perform API request without a token!"
-		return
-	}
-	if {![string is integer -strict $::strava::club_id] || \
-		[expr $::strava::club_id <= 0]} \
-	{
-		irssi_print "strava: club_activities: you must set a club id"
+		irssi_print "strava: api_request: cannot perform API request without a token!"
 		return
 	}
 	set headers [list "Authorization" "Bearer $::strava::oauth_token"]
-	set url [join [list $::strava::base_url "clubs" $::strava::club_id "activities"] "/"]
 	# set a useragent prior to every request. why? because many scripts set this
 	# and we could end up with an unknown useragent if we don't. this is
 	# apparently a limitation with global state in the http package.
 	::http::config -useragent $::strava::useragent
 	set http_token [::http::geturl $url -headers $headers \
 		-timeout [expr $::strava::http_timeout * 1000] \
-		-command ::strava::club_activities_cb]
+		-command $cb]
+}
+
+# start an http request to retrieve and output club activities.
+# this is asynchronous so we don't block.
+#
+# returns nothing.
+proc ::strava::club_activities {} {
+	if {![string is integer -strict $::strava::club_id] || \
+		[expr $::strava::club_id <= 0]} \
+	{
+		irssi_print "strava: api_request: you must set a club id"
+		return
+	}
+	set url [join [list $::strava::base_url "clubs" $::strava::club_id "activities"] "/"]
+	::strava::api_request $url ::strava::club_activities_cb
 }
 
 ::strava::load_config
